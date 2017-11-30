f7bba6bb60fb0e9f2761c639208c23d2db2fde9c
lxd <i@lxd.me>
2017-11-30 17:55:19 +1300

    initial commit

---
+++ src/main/java/co/nz/solnet/demo/kafka/consumernormal/ConsumerNormalApplication.java
@@ -0,0 +1,12 @@
+package co.nz.solnet.demo.kafka.consumernormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerNormalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerNormalApplication.class, args);
+	}
+}
